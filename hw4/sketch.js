2fdc1f312859758647e33b459709ace482fa4804
ghdud2583 <ghdud2583@gmail.com>
2025-11-28 15:26:24 +0900

    Create sketch.js

---
+++ hw4/sketch.js
@@ -0,0 +1,13 @@
+let startColor, endColor;
+
+function setup() {
+  createCanvas(800, 500);
+  angleMode(DEGREES);
+  colorMode(RGB);
+
+  startColor = color(255, 0, 0);
+  endColor = color(255, 255, 0);
+}
+
+function draw() {
+  ba
